5095590e1108f95801f83780cae25804ee96b466
Amerikranian <olegjpittman@gmail.com>
2026-05-14 19:50:25 -0500

    Bump version

--- MonsterTrainAccessibility/MonsterTrainAccessibility.cs
+++ MonsterTrainAccessibility/MonsterTrainAccessibility.cs
@@ -22,7 +22,7 @@ namespace MonsterTrainAccessibility
     {
         public const string GUID = "com.accessibility.monstertrain";
         public const string NAME = "Monster Train Accessibility";
-        public const string VERSION = "0.1.7";
+        public const string VERSION = "0.1.8";
 
         private Harmony _harmony;
         private AccessibleParamsManager _accessibleParamsManager;
